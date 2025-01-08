1e0312d191dece0dc25a312321e2ea3e5446e7e3
ComputerVIP <vincent.johnson@ucas-edu.net>
2025-01-08 13:43:38 -0700

    Started thingy

--- Financial Calculator/main.py
+++ Financial Calculator/main.py
@@ -9,8 +9,31 @@ Tip Calculator
 '''
 
 def add():
+    ans = float(input("What's your goal?\n"))
+    ans2 = float(input("How much money depositted per week?\n"))
+    print("This is how many weeks you need to save:", round((ans/ans2), 1))
+def interest():
+    ans = int(input("How much interest? (3% is 3)"))
+    ans2 = float(input("How much do you have in your account?"))
+def budget():
     pass
-
+def sale():
+    pass
+def tip():
+    pass
 def main():
-    ans = input(int("What would you like to do?\n1 for time to goal\n2 for interest\n3 for budget allocator\n4 for sale price\n5 for tip calculator"))
+    ans = int(input("What would you like to do?\n1 for time to goal\n2 for interest\n3 for budget allocator\n4 for sale price\n5 for tip calculator\n"))
     if ans == 1:
+        add()
+    elif ans == 2:
+        interest()
+    elif ans == 3:
+        budget()
+    elif ans == 4:
+        sale()
+    elif ans == 5:
+        tip()
+    else:
+        return
+    
+main()
