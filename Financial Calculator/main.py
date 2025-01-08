de8612055e2d22e04319102f75914c2a85753919
ComputerVIP <vincent.johnson@ucas-edu.net>
2025-01-08 13:31:44 -0700

    added pass function

---
+++ Financial Calculator/main.py
@@ -0,0 +1,16 @@
+#Vincent Johnson financial calculator
+#ms larse did a thing
+'''
+How long it will take to save for a goal based on a weekly or monthly deposit
+Compound Interest Calculator 
+Budget Allocator (use set percentages to divide an income into spending categories like savings, entertainment, and food)
+Sale Price Calculator (apply discounts to prices)
+Tip Calculator
+'''
+
+def add():
+    pass
+
+def main():
+    ans = input(int("What would you like to do?\n1 for time to goal\n2 for interest\n3 for budget allocator\n4 for sale price\n5 for tip calculator"))
+    if ans == 1:
