d38d7c4927949d190ebfee1f3c5ebc201de649a3
ComputerVIP <vincent.johnson@ucas-edu.net>
2025-01-10 13:13:22 -0700

    Shoved in a print statement

--- Personal/#Using PyBrain AI.py
+++ Personal/#Using PyBrain AI.py
@@ -1,6 +1,6 @@
 #Using PyBrain AI
 # https://www.tutorialspoint.com/pybrain/introduction_to_pybrain_networks.htm
 
-from pybrain.tools.shortcuts
-import buildNetwork
-Net = buildNetwork(2, 3, 1)
+from pybrain.tools.shortcuts import buildNetwork
+Network = buildNetwork(2, 3, 1)
+print(Network)
