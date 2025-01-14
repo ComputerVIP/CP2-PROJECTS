84d55dad2a12ceb4a452eff0a104814b33c00a82
ComputerVIP <vincent.johnson@ucas-edu.net>
2025-01-13 20:55:06 -0700

    Added and removed folders

--- Personal/#Using PyBrain AI.py
+++
@@ -1,6 +0,0 @@
-#Using PyBrain AI
-# https://www.tutorialspoint.com/pybrain/introduction_to_pybrain_networks.htm
-
-from pybrain.tools.shortcuts import buildNetwork
-Network = buildNetwork(2, 3, 1)
-print(Network)
