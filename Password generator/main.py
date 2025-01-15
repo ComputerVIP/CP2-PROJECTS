e5efb2684a0c38374a5dc4c983b4886e2fddc729
ComputerVIP <vincent.johnson@ucas-edu.net>
2025-01-14 20:44:23 -0700

    Started and finished two assignments

---
+++ Password generator/main.py
@@ -0,0 +1,98 @@
+#Vincent Johnson
+#ms larse did a thing
+#-Ms. Larose
+
+'''
+A main function that runs the code
+Functions for the different password requirements
+A function that assembles that password once it is the correct length
+Users should be able to specify length and if they want to include
+uppercase letters
+lowercase letters
+numbers
+special characters
+'''
+import random
+import string
+
+def start():
+    def upp():
+        cnt = 5
+        lst = list(string.ascii_lowercase) + list(string.ascii_uppercase)
+        al = ""
+        ans = int(input("How long do you want your password?\n"))
+        while cnt > 1:
+            while len(al) < ans:
+                al = al + lst[(random.randint(0, len(lst)))]
+            print("Possible password:", al)
+            al = ""
+            cnt -= 1
+        return
+    def low():
+        cnt = 5
+        lst = list(string.ascii_lowercase)
+        al = ""
+        ans = int(input("How long do you want your password?\n"))
+        while cnt > 1:
+            while len(al) < ans:
+                al = al + lst[(random.randint(0, len(lst)))]
+            print("Possible password:", al)
+            al = ""
+            cnt -= 1
+        return
+    def spcl():
+        cnt = 5
+        lst = list(string.ascii_lowercase) + list(string.ascii_uppercase) + list(string.punctuation)
+        al = ""
+        ans = int(input("How long do you want your password?\n"))
+        while cnt > 1:
+            while len(al) < ans:
+                al = al + lst[(random.randint(0, len(lst)))]
+            print("Possible password:", al)
+            al = ""
+            cnt -= 1
+        return
+    def numb():
+        cnt = 5
+        lst = list(string.ascii_lowercase) + list(string.ascii_uppercase) + list(string.digits)
+        al = ""
+        ans = int(input("How long do you want your password?\n"))
+        while cnt > 1:
+            while len(al) < ans:
+                al = al + lst[(random.randint(0, len(lst)))]
+            print("Possible password:", al)
+            al = ""
+            cnt -= 1
+        return
+    def all():
+        cnt = 5
+        lst = list(string.ascii_lowercase) + list(string.ascii_uppercase) + list(string.digits) + list(string.punctuation)
+        al = ""
+        ans = int(input("How long do you want your password?\n(Longer passwords work better)\n"))
+        while cnt > 1:
+            while len(al) < ans:
+                al = al + lst[(random.randint(0, len(lst)))]
+            print("Possible password:", al)
+            al = ""
+            cnt -= 1
+        return
+    print("\nWhat would you like to do?\n 1 for lowercase only\n 2 for uppercase and lowercase")
+    ans = int(input(" 3 for special characters\n 4 for numbers and letters\n 5 for all\n"))
+    if ans == 1:
+        low()
+        return start()
+    elif ans == 2:
+        upp()
+        return start()
+    elif ans == 3:
+        spcl()
+        return start()
+    elif ans == 4:
+        numb()
+        return start()
+    elif ans == 5:
+        all()
+        return start()
+    else:
+        return ("Goodbye!")
+start()
--- Personal Library/main.py
+++ Personal Library/main.py
@@ -1,5 +1,7 @@
 #Vincent Johnson personal library
 #ms larse did a thing
+#-Ms. Larose
+
 '''
 Stores all items in a list
 Function to add a new item
@@ -12,9 +14,60 @@ Pseudocode comments explaining what the code is doing
 Good use of white space to keep items separate and easy to read/find
 Have at least 2 people test your code before submission!
 '''
-def main():
-    cat = []
-    title = input("What kind of thing is going to be stored in this library?")
-    rpt = int(input("How many things do you want to add?"))
-    while rpt > 0:
-        ad = input("What")
+
+#Cat is my list, inspired by my friend Zoe who sent me a photo of her cat recently
+cat = []
+def start(cat):
+
+    #This function just finds the index of the item, and adds one to show what position it is in a list to a normal person, it then prints the list
+    def srch(cat):
+        ans = input("What would you like to search for?\n")
+        res = (cat.index(ans)) + 1
+        print(f"{cat}\n{ans} is {res} in the list")
+        return cat
+    
+    #This function just repeats for how many things you need to get rid of, the .pop's them, tried eror handling, I don't know why it didn't work
+    def remove(cat):
+        rpt = int(input("How many things do you want to remove from your list?\n"))
+        print(f"rpt: {rpt}\n")
+        shw = 0
+        while shw < rpt:
+            shw += 1
+            ad = input(f"What item do you want to remove?\n({shw}/{rpt})\n")
+            try:
+                cat.pop(ad)
+            except:
+                print("exception\n")
+                return ("Error occurred")
+        print(f"\n\n{cat}")
+        return cat
+        
+    #This function just asks for how many times to repeat, then adds you input to the list and prints it
+    def add(cat):
+        rpt = int(input("How many things do you want to add to your list?\n"))
+        shw = 0
+        while shw < rpt:
+            shw += 1
+            ad = input(f"What item do you want to add?\n({shw}/{rpt})\n")
+            cat.append(ad)
+        print(f"\n\n{cat}")
+        return cat
+    
+    #This is the new and improved selection menu!
+    #This is inside the big main function, and will ALWAYS start a new big main function, stopping any weird errors with that
+    #It also allows for easier exiting, and allows me to export my variables seamlessly
+    #It just has a list of options and whichever you pick, it runs that function
+    ans = int(input("\nWhat would you like to do?\n 1 for add to list\n 2 for remove items\n 3 for search\n 4 for exit\n"))
+    if ans == 1:
+        add(cat)
+        return start(cat)
+    elif ans == 2:
+        remove(cat)
+        return start(cat)
+    elif ans == 3:
+        srch(cat)
+        return start(cat)
+    else:
+        return ("Goodbye!")
+#Starts the program
+start(cat)
