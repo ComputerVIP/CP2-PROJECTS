b10dc178472b2c188d9a5e4856c3c91de36ba6c0
ComputerVIP <vincent.johnson@ucas-edu.net>
2025-02-03 19:02:33 -0700

    Finished the assignment

--- Test.py
+++ Test.py
@@ -2,15 +2,19 @@ import keyboard
 import time
 cnt = 0
 cnt2 = []
-time.sleep(5)
-while cnt < 2:
-    nm = 47 + cnt
+time.sleep(4.5)
+
+while cnt < 100:
+    time.sleep(0.5)
+    nm = 48 + cnt
     for x in cnt2:
-        keyboard.write("    ")
+        #keyboard.write("    ")
+        time.sleep(0.5)
     keyboard.write(f'''"lst{nm}": {{}}''')
+    time.sleep(0.5)
     keyboard.send("left_arrow")
+    time.sleep(1)
     keyboard.send("enter")
     cnt2.append(cnt)
     print(cnt)
     cnt += 1
-
