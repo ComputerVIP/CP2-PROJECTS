2e4a6d8bc175686fe1362077a220a8f0faad7229
ComputerVIP <vincent.johnson@ucas-edu.net>
2025-02-10 17:32:26 -0700

    Fixed

--- Personal Library-Update/main.py
+++ Personal Library-Update/main.py
@@ -16,17 +16,15 @@ Good use of white space to keep items separate and easy to read/find
 Have at least 2 people test your code before submission!
 '''
 
-lst = {
-    
-}
+lst = []
 
 cnt = 1
 
 # This function just finds the index of the item and adds one to show what position it is in a list to a normal person, then it prints the list
 def srch(lst, cnt):
     ans = input("What would you like to search for?\n").upper()
-    for key, values in lst.items():
-        if ans in values:
+    for i in lst:
+        if ans in i:
             print(f"{ans} is in the list")
     ans = int(input(" 1 for menu\n 2 for end\n"))
     if ans == 1:
@@ -38,14 +36,11 @@ def srch(lst, cnt):
 
 # This function just repeats for how many things you need to get rid of, and removes them from the list
 def remove(lst, cnt):
-    rpt = int(input("How many things do you want to remove from your list?\n"))
-    shw = 0
-    while shw < rpt:
-        print(lst)
-        shw += 1
-        key1 = input(f"What kind of item is going to be removed?\n({shw}/{rpt})\n").upper()
-        inpt = input(f"What would you like to remove?\n").upper()
-        lst[key1] = [x for x in lst[key1] if x != inpt]  
+    ans = input("What would you like to remove?\n")
+    for i in lst:
+        if ans in i:
+            lst = lst.remove(i)
+    print(lst)
 
     print(f"\n\n{lst}")
     ans = int(input(" 1 for menu\n 2 for end\n"))
@@ -58,11 +53,10 @@ def remove(lst, cnt):
 
 # This function asks for how many times to repeat, then adds your input to the list and prints it
 def add(lst, cnt):
-    rpt = int(input("How many things do you want to add to your list?\n"))
+    rpt = 4
     shw = 0
-    while shw < rpt:
-        shw += 1
-        lst.setdefault((input(f"What kind of item is going to be added?\n({shw}/{rpt})\n").upper()), []).append(input(f"What item do you want to add?\n").upper())
+    lst.append({(input("What is the name of the item?\n").upper): [(input("What is one thing you would like to mention about it?\n").upper()), (input("What is a second thing you would like to mention about it?\n").upper()), (input("What is a third thing you would like to mention about it?\n").upper()), (input("What is a fourth thing you would like to mention about it?\n").upper())]})
+    #lst.setdefault((input(f"What kind of item is going to be added?\n({shw}/{rpt})\n").upper()), []).append(input(f"What item do you want to add?\n").upper())
     print(f"\n\n{lst}")
     ans = int(input(" 1 for menu\n 2 for end\n"))
     if ans == 1:
@@ -88,13 +82,29 @@ def main(lst, cnt):
     while cnt > 0:  # Ensure the main loop only runs if cnt > 0
         ans = int(input("\nWhat would you like to do?\n 1 for add to list\n 2 for remove items\n 3 for search\n 4 view list\n 5 for exit\n"))
         if ans == 1:
-            cnt, lst = add(lst, cnt)
+            try:
+                cnt, lst = add(lst, cnt)
+            except:
+                lst = []
+                print("Error occurred, list reset")
         elif ans == 2:
-            cnt, lst = remove(lst, cnt)
+            try:
+                cnt, lst = remove(lst, cnt)
+            except:
+                lst = []
+                print("Error occurred, list reset")
         elif ans == 3:
-            cnt, lst = srch(lst, cnt)
+            try:
+                cnt, lst = srch(lst, cnt)
+            except:
+                lst = []
+                print("Error occurred, list reset")
         elif ans == 4:
-            cnt, lst = view(lst, cnt)
+            try:
+                cnt, lst = view(lst, cnt)
+            except:
+                lst = []
+                print("Error occurred, list reset")
         elif ans == 5:
             print("Goodbye!")
             cnt = 0
