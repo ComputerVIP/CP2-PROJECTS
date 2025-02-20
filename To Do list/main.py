c547bae05b34e9d16605501c859f31fa5da92924
ComputerVIP <vincent.johnson@ucas-edu.net>
2025-02-20 13:44:10 -0700

    Fixed!

--- To Do list/main.py
+++ To Do list/main.py
@@ -8,29 +8,57 @@ Delete item from to do list
 '''
 
 repeat = 1
+#res = {}
 
 def add():
     with open('To Do list/List.txt', 'r') as file:
         try:
             res = dict(line.strip().split(':', 1) for line in file)
         except:
-            try:
-                next(file.read())
+            try: 
                 res = dict(line.strip().split(':', 1) for line in file)
             except:
                 pass
-    with open("To Do list/List.txt", "w+") as file:
-        print(res)
-        ad = input("What would you like to add?\n")
-        res[ad] = ''
-        print(res)
+    with open("To Do list/List.txt", "w") as file:
+        try:
+            for key, values in res.items():
+                print(f"{key}: {values}")
+        except:
+            pass
+        ad = input("What would you like to add?\n").upper()
+        res[ad] = 'TODO'
         for key, value in res.items():
-            file.write(f"\n{key}: {value}")
-        for row in file.read():
-            print(row)
+                file.write(f"\n{key}:{value}\n")
+        for key, values in res.items():
+            print(f"{key}: {values}")
+    return
+def mark():
+    with open('To Do list/List.txt', 'r') as file:
+        try:
+            res = dict(line.strip().split(':', 1) for line in file)
+        except:
+            try: 
+                res = dict(line.strip().split(':', 1) for line in file)
+            except:
+                pass
+    with open("To Do list/List.txt", "w") as file:
+        try:
+            for key, values in res.items():
+                print(f"{key}: {values}")
+        except:
+            pass
+        ad = input("What would you like to mark as completed?\n").upper()
+        res[ad] = 'COMPLETED'
+        for key, value in res.items():
+            file.write(f"\n{key}:{value}")
+        for key, values in res.items():
+            print(f"{key}: {values}")
 def main(repeat):
     ans = input("What would you like to do?\n    1 for Adding items\n    2 for marking items as completed\n    3 for deleting items\n    4 for exit\n")
     if ans == "1":
         add()
+    if ans == "2":
+        mark()
+    return repeat
 while repeat > 0:
     main(repeat)
